0c31bd1ec5fb38a48bc617c80c055190c7bff69c
mr <mr@gmail.com>
2021-10-01 15:15:12 +0530

    reponsiveDone

--- src/app/layout/header/category-menu-bar/category-menu-bar.component.ts
+++ src/app/layout/header/category-menu-bar/category-menu-bar.component.ts
@@ -5,6 +5,7 @@ import * as _ from "lodash";
 import { Router } from '@angular/router';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { environment } from 'src/environments/environment';
+import * as $ from "jquery"
 @Component({
   selector: 'yaari-category-menu-bar',
   templateUrl: './category-menu-bar.component.html',
@@ -33,6 +34,33 @@ export class CategoryMenuBarComponent implements OnInit {
     //   $(".menu > ul").toggleClass('show-on-mobile');
     //   e.preventDefault();
     // });
+    
+    $(document).ready(function() {
+      "use strict";
+      $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+      $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+      $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\">&nbsp;</a>");
+      $(".menu > ul > li").hover(function(e) {
+        if ($(window).width() > 943) {
+          $(this).children("ul").stop(true, false).fadeToggle(150);
+          e.preventDefault();
+        }
+      });
+      $(".menu > ul > li").click(function() {
+        if ($(window).width() <= 943) {
+          $(this).children("ul").fadeToggle(150);
+        }
+      });
+      $(".menu-mobile").click(function(e) {
+        $(".menu > ul").toggleClass('show-on-mobile');
+        e.preventDefault();
+      });
+    });
+    $(window).resize(function() {
+      $(".menu > ul > li").children("ul").hide();
+      $(".menu > ul").removeClass('show-on-mobile');
+    });
+    
     // setTimeout(()=>{
       this.getAllCollection();
       this.getAllCategory();
